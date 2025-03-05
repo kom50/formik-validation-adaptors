b622a753902ebb4dfcefe149009f3c25bdf387dd
kom50 <omprakashkartik82280@gmail.com>
2025-03-05 22:33:22 +0530

    Added test coverage feature for testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,20 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {},
+  test: {
+    coverage: {
+      provider: "istanbul", // or 'v8'
+      reporter: ["text"], // Generates coverage report in terminal and as an HTML file
+      all: true,
+      exclude: [
+        "node_modules",
+        "dist",
+        "coverage",
+        ".github",
+        "examples",
+        "vitest.config.ts",
+      ],
+    },
+    reporters: ["default", "verbose"], // Show detailed test cases
+  },
 });
